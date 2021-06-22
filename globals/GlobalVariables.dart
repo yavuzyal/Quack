c255c3e90e9c0b727706978ee493504dd2dbdd6d
yavuzyal <51164676+yavuzyal@users.noreply.github.com>
2021-06-22 15:47:46 +0200

    Add files via upload

---
+++ globals/GlobalVariables.dart
@@ -0,0 +1,13 @@
+library cs308_project.globals;
+
+String accessToken = "";
+
+bool isLoggedIn = false;
+
+String cookie;
+
+List<String> nonLoggedInItems = [];
+
+
+// ignore: non_constant_identifier_names
+bool credit_card_entered = false;
